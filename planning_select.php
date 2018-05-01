02af08ee0735a0783d6a9bb00c497cb865bda2a1
liavtavo <thomasliautaud94@gmail.com>
2018-05-02 01:27:06 +0200

    modifiaction de la forme de planning et ajouts de deux nouvelles page pour faire des sélections sur le planning

--- planning_select.php
+++ planning_select.php
@@ -7,9 +7,9 @@
 
 <body>
 <a href="accueil.html">Retour à l'accueil</a>
-<h2>Planning des prélèvements</h2>
+<h2>Planning des points de prélèvements sélectionnés</h2>
 
-<a href=#planning>Planning des hebdomadaire des points de prélèvements</a>
+<!-- <a href=#planning>Planning des points de prélèvements</a> --!>
 
 <!-- connection à la bdd -->
 
@@ -20,23 +20,33 @@ if ($a==false)
 	echo "problème de connexion<br>";
 	exit;
 }
+
+$jour=$_POST['jour'];
+$classe=$_POST['classe'];
+$type=$_POST['type'];
+
+echo "Jour : ".$jour."<br>";
+echo "classe : ".$classe."<br>";
+echo "type : ".$type."<br>";
+echo "<p>";
+
 $question="select * from planning_prelev";
 
 $reponse=pg_query($a, $question);
 if ($reponse==false)
 {
-	echo "problème<br>";
+	echo "problème de connection à la vue<br>";
 }
 
 $colonnes=pg_num_fields($reponse);
 $lignes=pg_numrows($reponse);
 
-$question="SELECT jour, classe, type, point, description FROM planning_prelev, limites_classes, points_prelev, jours_prelev WHERE jours_prelev.id=planning_prelev.id_jour AND points_prelev.id=planning_prelev.id_point AND points_prelev.id_class=limites_classes.id;";
+$question="SELECT jour, classe, type, point, description FROM planning_prelev, limites_classes, points_prelev, jours_prelev WHERE jours_prelev.id=planning_prelev.id_jour AND points_prelev.id=planning_prelev.id_point AND points_prelev.id_class=limites_classes.id AND jour LIKE '$jour' AND classe LIKE '$classe' AND type LIKE '$type';";
 
 $reponse=pg_query($a, $question);
 if ($reponse==false)
 {
-	echo "problème<br>";
+	echo "problème de requête<br>";
 }
 
 echo "<p>";
@@ -44,7 +54,8 @@ echo "<p>";
 $colonnes=pg_num_fields($reponse);
 $lignes=pg_numrows($reponse);
 
-echo '<table id="planning"><caption>Planning hebdomadaire des points de prélèvements</caption>';
+echo '<table id="planning"><caption>Planning des points de prélèvements<br>
+					Jour : '.$jour.' - Classe : '.$classe.' - Type : '.$type.'</caption>';
 echo "<tr>";
 for ($i=0; $i<$colonnes;$i++)
 {
