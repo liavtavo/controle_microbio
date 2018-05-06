8d59796e80de39d7f75fc6b79df4336dde699ea2
liavtavo <thomasliautaud94@gmail.com>
2018-05-06 08:46:14 +0200

    début de création d'un palmares des points prélevés : palmares_prelevements.php

---
+++ palmares_prelevements.php
@@ -0,0 +1,70 @@
+<html>
+	<head>
+		<title>Prélèvements</title>
+		<meta http-equiv="content-type" content="text/html, charset=utf-8" />
+		<link rel="stylesheet" href="style.css" />
+	</head>
+
+<body>
+<a href="accueil.html">Retour à l'accueil</a>
+<h2>Prélèvements réalisés et résultats</h2>
+<br>
+<a href=prelevements_select.html>Filtrer</a>
+
+<!-- connection à la bdd -->
+<?php
+$a=pg_connect("dbname=bacterio_upnp user=pharmacien host=127.0.0.1 password=zac");
+if ($a==false)
+{
+	echo "problème de connexion<br>";
+	exit;
+}
+
+$question="select * from prelevements";
+
+$reponse=pg_query($a, $question);
+if ($reponse==false)
+{
+	echo "problème<br>";
+}
+
+$colonnes=pg_num_fields($reponse);
+$lignes=pg_numrows($reponse);
+
+
+$question='SELECT point, classe, COUNT(prelevements.id) AS nb FROM points_prelev, prelevements, limites_classes WHERE prelevements.id_point=points_prelev.id AND points_prelev.id_class=limites_classes.id GROUP BY point, classe ORDER BY nb DESC;';
+
+$reponse=pg_query($a, $question);
+if ($reponse==false)
+{
+	echo "problème<br>";
+}
+
+echo "<p>";
+
+$colonnes=pg_num_fields($reponse);
+$lignes=pg_numrows($reponse);
+
+echo "<table border id=#prelevements><caption>Table des prélèvements réalisés (du plus recent au plus ancien) et des résultats</caption>";
+echo "<tr>";
+for ($i=0; $i<$colonnes;$i++)
+{
+	echo "<th>".pg_field_name($reponse, $i)."</th>";
+}
+echo"</tr>";
+
+for ($j=0; $j<$lignes; $j++)
+{
+	echo "<tr>";
+	$uneligne=pg_fetch_array($reponse,$j);
+	echo "<td>".$uneligne['No']."</td><td>".$uneligne['date']."</td><td>".$uneligne['point']."</td><td>".$uneligne['type']."</td><td>".$uneligne['description']."</td><td>".$uneligne['classe']."</td><td>".$uneligne['ufc']."</td><td>".$uneligne['limite']."</td><td>".$uneligne['conformité']."</td><td>".$uneligne['germe']."</td><td>".$uneligne['téléphoné']."</td><td>".$uneligne['signé']."</td>";
+	echo "</tr>";
+}
+
+echo "</table>";
+
+?>
+
+
+</body>
+</html>
